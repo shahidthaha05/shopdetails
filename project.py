1c004a00cf3223a9e33f1c236d2ca0fb90c30dae
shahid <shahidthaha2005@gmail.com>
2024-09-04 09:30:54 +0530

    file added

---
+++ project.py
@@ -0,0 +1,67 @@
+veh=[['x5','bmw',2020,'petrol',20,1],['panamera','porsche',2022,'gas',25,2]]
+while True:
+ print('''
+1.vehicle details
+2.view vehicle details
+3.upadte vehicle details
+4.delete vehicle details
+5.search vehicle
+6.exit
+          ''')
+ 
+ choice=int(input("enter your choice :"))
+ if choice==1:
+         name=str(input("enter name :"))
+         brand=str(input("enter brand :"))
+         model=int(input("enter the model :"))
+         fueltype=str(input("enter fueltype :"))
+         mileage=int(input("enter the mileage :"))
+         insurance=int(input("enter the insurance :"))
+         veh.append([name,brand,model,fueltype,mileage,insurance])
+ elif choice==2:
+      for i in veh:
+           print(i)
+ 
+ elif choice==3:
+         name=str(input("enter name :"))
+         f=0
+         for i in veh:
+              if name in i:
+                   choice=int(input("enter your choice :"))
+                   if choice==1:
+                        brand=str(input("enter brand :"))
+                        i[1]=brand
+                   elif choice==2:
+                        model=int(input("enter model :"))
+                        i[2]=model
+                   elif choice==3:
+                        fueltype=str(input("enter fueltype  :"))
+                        i[3]=fueltype
+                   elif choice==4:
+                        mileage=str(input("enter mileage :"))
+                        i[4]=mileage
+                   elif choice==5:
+                        insurance=int(input("enter insurance :"))
+                        i[5]=insurance
+                   f=1           
+         if f==0:
+              print('invalid name') 
+ elif choice==4:
+      name=str(input("enter name :"))
+      f=0
+      for i in veh:
+           if name in i:
+                veh.remove(i)
+                f=1
+      if f==0:
+           print('invalid name')
+ elif choice==5:
+      name=str(input("enter name :"))
+      f=0
+      for i in veh:
+           if name in i:
+                print(i)
+ elif choice==6:
+     break
+ else:
+     print('invalid choice')
